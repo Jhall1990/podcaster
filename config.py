6c5a290d0f03bf890d2a920e708ec8892eab385c
Jacob Hall <jacobhall90@gmail.com>
2023-01-21 17:42:20 +0000

    Fix config

--- config.py
+++ config.py
@@ -1,32 +1,32 @@
 # The rss feed for the youtube channel to convert to podcast.
 # Format should be something like https://www.youtube.com/feeds/videos.xml?channel_id=<channel_id>"
 # You can get the <channel_id> from the channel's page on youtube.
-YOUTUBE_RSS = "https://www.youtube.com/feeds/videos.xml?channel_id=UCpXBGqwsBkpvcYjsJBQ7LEQ"
+YOUTUBE_RSS = ""
 
 # The regex to identify episodes to convert to podcast.
-EPISODE_RE = r"(.*?)\s\|\sCritical Role\s\|\sCampaign\s\d+,\sEpisode\s(\d+)"
+EPISODE_RE = r""
 
 # The location (folder) where the downloaded episodes should be saved.
 # Example: /home/user/downloads/
-EPISODE_LOCATION = "/mnt/nas/storage/ftp/programs/podcaster/episodes"
+EPISODE_LOCATION = ""
 
 # The location (file) where the podcast rss file should be saved.
 # /home/user/podcast/pod.xml
-XML_LOCATION = "/mnt/nas/storage/ftp/programs/podcaster/pod.xml"
+XML_LOCATION = ""
 
 # The url prefix where the episodes will be hosted.
 # Example: http://192.168.1.100/episodes
-EPISODE_URL_PREFIX = "http://freya.home:81/storage/ftp/programs/podcaster/episodes/"
+EPISODE_URL_PREFIX = ""
 
 # Metadata about your podcast, this will be put into the generated podcast xml (rss)
 # file. Most of this doesn't really matter as you're probably not putting this podcast
 # out for the rest of the world to see.
 PODCAST_INFO = {
-        "title": "Local Critical Role",
-        "url": "youtube.com",
-        "desc": "Critical Role",
-        "category": "DND",
-        "email": "jacobhall90@gmail.com"
+        "title": "Title",
+        "url": "url",
+        "desc": "desc",
+        "category": "category",
+        "email": "email"
     }
 
 # Whether or not to use sponsor block to remove ads from the audio.
